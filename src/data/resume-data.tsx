395171e0cb1189141ac9cdee398ee1095191a9f1
Junaid Khan <dianujkotov15@gmail.com>
2023-12-31 14:51:01 +0900

    first commit

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -44,11 +44,11 @@ export const RESUME_DATA = {
         icon: GitHubIcon,
       },
       // create google scholar icon
-      // {
-      //   name: "Google Scholar",
-      //   url: "https://scholar.google.com/citations?user=03BvsCEAAAAJ&hl=en",
-      //   icon: GoogleScholarIcon
-      // },
+      {
+        name: "Google Scholar",
+        url: "https://scholar.google.com/citations?user=03BvsCEAAAAJ&hl=en",
+        icon: GoogleScholarIcon
+      },
 
       // {
       //   name: "LinkedIn",
