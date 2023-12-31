2e7fdf28be3c11818f6e65c91048cfdbeff00194
Junaid Khan <dianujkotov15@gmail.com>
2023-12-31 16:22:13 +0900

    first commit

--- src/components/icons/GS.tsx
+++ src/components/icons/GS.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
-export const GraduationCapIcon = (props: React.SVGProps<SVGSVGElement>) => {
+export const GS = (props: React.SVGProps<SVGSVGElement>) => {
     return (
-      <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" {...props}>
-        <path
-          fill="currentColor"
-          d="M12,1 L1,7 l11,6 l11,-6 L12,1 z M12,15 l8,0 0,-8 -8,4 -8,-4 0,8 8,0 z"
-        />
-      </svg>
-    );
-  };
+        <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" {...props}>
+          <path
+            fill="currentColor"
+            d="M12,1 L1,7 l11,6 l11,-6 L12,1 z M12,15 l8,0 0,-8 -8,4 -8,-4 0,8 8,0 z"
+          />
+        </svg>
+      );
+};
