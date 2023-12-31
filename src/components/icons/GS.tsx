bd8ec17a8b0a6d255f383687dd9a75d7f2014c20
Junaid Khan <dianujkotov15@gmail.com>
2023-12-31 16:18:53 +0900

    first commit

--- src/components/icons/GS.tsx
+++ src/components/icons/GS.tsx
@@ -5,7 +5,7 @@ export const GS = (props: React.SVGProps<SVGSVGElement>) => {
     <svg viewBox="0 0 20 24" xmlns="http://www.w3.org/2000/svg" {...props}>
       <path
         fill="currentColor"
-        d="M2,12l10-7l10,7l-10,7L2,12z M12,5v13"
+        d="M12,2 L2,8 l10,6 l10,-6 L12,2 z M12,14 l6,0 0,-6 -6,3 -6,-3 0,6 6,0 z"
       />
     </svg>
   );
