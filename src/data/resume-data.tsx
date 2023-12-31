156fbdc973eae2a6738a6b188ba4a3983dbb8c55
Junaid Khan <dianujkotov15@gmail.com>
2023-12-31 14:32:52 +0900

    first commit

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -29,7 +29,7 @@ export const RESUME_DATA = {
   summary:
     "As a Full Stack Engineer, I have successfully taken multiple products from 0 to 1. I lead teams effectively, ensuring an environment where people can do their best work. Currently, I work mostly with TypeScript, React, Node.js, and GraphQL. I have over 8 years of experience in working remotely with companies all around the world.",
   avatarUrl: "https://avatars.githubusercontent.com/u/47445756?v=4",
-  personalWebsiteUrl: "https://jarocki.me",
+  // personalWebsiteUrl: "https://jarocki.me",
   contact: {
     email: "bartosz.jarocki@hey.com",
     tel: "+48530213401",
