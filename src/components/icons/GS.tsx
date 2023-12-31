f031b5461ba129c84c3c1d8f9b698fcd389794d9
Junaid Khan <dianujkotov15@gmail.com>
2023-12-31 15:05:14 +0900

    first commit

--- src/components/icons/GS.tsx
+++ src/components/icons/GS.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-export const GoogleScholarIcon = (props: React.SVGProps<SVGSVGElement>) => {
+export const ScholarIcon = (props: React.SVGProps<SVGSVGElement>) => {
   return (
     <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" {...props}>
       <path
         fill="currentColor"
-        d="/images/logos/parabol.svg"
+        d="M12,2L1,7l11,5l11-5L12,2z M12,13L1,8v6l11,5l11-5V8L12,13z"
       />
     </svg>
   );
